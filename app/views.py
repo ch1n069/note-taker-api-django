3dfea42fec81b297344595d2fc371bbbefc4eb59
Bruno Alpachino <brunolalpachino@gmail.com>
2022-09-20 19:32:30 +0300

    used viewsets for the viewfunctions

--- app/views.py
+++ app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from app.models import Note
+from rest_framework import viewsets
+from app.serializers import NoteSerializer
 # Create your views here.
+# the view sets allow us to create the http responses
+
+
+class NotesViewset(viewsets.ModelViewset):
+    '''this is responsible for fetching all the notes '''
+    serializer =  NoteSerializer
+    queryset = Note.objects.all()

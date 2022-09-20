2c1c8ad7aab83583b164dbb9cc60cf208680a880
Bruno Alpachino <brunolalpachino@gmail.com>
2022-09-20 19:09:32 +0300

    created the notes model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        self.title
+        
+
